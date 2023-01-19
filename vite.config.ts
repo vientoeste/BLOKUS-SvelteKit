0d2991b9d32a24dfa296e9d776c25ba38c396d97
este <djjd2000@naver.com>
2023-01-20 03:11:14 +0900

    Feature: added socket.io initializing logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { Server } from 'socket.io';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), {
+		name: 'wss',
+		configureServer(server) {
+			if (server.httpServer) {
+				const io = new Server(server.httpServer);
+				// [TODO] io.on comes here
+				console.log('wss opened');
+			}
+		}
+	}]
 };
 
 export default config;
