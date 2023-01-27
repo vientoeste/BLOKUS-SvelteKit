7666de34d1e67681893858ab2a40cb10d2f3e4c9
este <djjd2000@naver.com>
2023-01-27 13:42:41 +0900

    S/F: added blocks next to board and logic to send position through form tag

--- src/routes/game/[room_uuid]/+page.server.ts
+++ src/routes/game/[room_uuid]/+page.server.ts
@@ -1,13 +1,26 @@
 import type { PageServerLoad } from "./$types";
 import { validate } from "uuid";
 import db from '$lib/database';
+import { BLOCK } from "../game";
+import type { Actions } from "@sveltejs/kit";
 
 const makeTableHead = (): string => {
   let tableHead = '';
   for (let i = 0; i < 20; i++) {
-    tableHead = tableHead.concat(`<th>${i % 10}</th>`);
+    tableHead = tableHead.concat(`<th style="border: 1px solid rgba(0, 0, 0, 0.267);">${i % 10}</th>`);
   }
   return tableHead;
+};
+
+const makeTableContents = (e: (string | number)[][]) => {
+  return `<div class="blocks">
+    <table style="border: 10px;">`.concat(e.reduce((prev: string, curr: (string | number)[]) => {
+    const currentRow = curr.reduce((
+      prevShadow: string, currShadow: (string | number)
+    ) => prevShadow.concat(currShadow === 0 ? '<th></th>' : `<td><div class='a'>\u00a0</div></td>`), '');
+
+    return prev.concat(currentRow, '</tr>');
+  }, ''), `</table></div>`)
 };
 
 export const load = (async ({ params }) => {
@@ -27,15 +40,29 @@ export const load = (async ({ params }) => {
 
   board = board.reduce((prev: string, curr: (string | number)[], idx: number) => {
     const currentRow = curr.reduce((
-      prevShadow: string, currShadow: (string | number)
-    ) => prevShadow.concat(`<td><div class=${currShadow === 0
-      ? 'none'
-      : currShadow}>\u00a0</div></td>`), '');
+      prevShadow: string, currShadow: (string | number), idxShadow: number
+    ) => prevShadow.concat(`<td onClick="document.getElementById('row').value='${idx}'; document.getElementById('col').value='${idxShadow}'; document.getElementById('tmp').submit();">
+    <div class=${currShadow === 0
+        ? 'none'
+        : currShadow}>\u00a0</div></td>`), '');
 
-    return prev.concat(`<tr><th>${idx}</th>`, currentRow, '</tr>');
+    return prev.concat(`<tr><th style="border: 1px solid rgba(0, 0, 0, 0.267);">${idx % 10}</th>`, currentRow, '</tr>');
   }, '');
-
   return {
     board: `<tr><th />${makeTableHead()}</tr>`.concat(board),
+    block: makeTableContents(BLOCK.five.d)
   };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  putBlock: async (event) => {
+    const position = await event.request.formData().then((e) => {
+      return [
+        e.get('row') as string,
+        e.get('col') as string,
+      ]
+    });
+    console.log(position)
+    return 'ok';
+  }
+} satisfies Actions;
--- src/routes/game/[room_uuid]/+page.svelte
+++ src/routes/game/[room_uuid]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { prevent_default } from "svelte/internal";
   import type { PageData } from "./$types";
   export let data: PageData;
 </script>
@@ -8,6 +9,11 @@
 </head>
 <br />
 <br />
-<table style="border: 10px;">
-  {@html data.board}
-</table>
+<form method="POST" id="tmp" action="?/putBlock">
+  <table style="border: 1px solid rgba(0, 0, 0, 0.267);" class="board">
+    {@html data.board}
+  </table>
+  <input type="hidden" id="row" name="row" value="" />
+  <input type="hidden" id="col" name="col" value="" />
+</form>
+{@html data.block}
