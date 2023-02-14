d226a81c862797789e6e967ebe583ef2550ca2cf
este <djjd2000@naver.com>
2023-02-14 23:22:20 +0900

    Feature: setting websocket server with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,48 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import type { UserConfig, ViteDevServer } from 'vite';
 import WebSocket, { WebSocketServer } from 'ws';
 
-const config: UserConfig = {
-	plugins: [sveltekit(), {
-		name: 'wss',
-		configureServer(server) {
-			if (server.httpServer) {
-				// const io = new Server(server.httpServer);
-				// [TODO] io.on comes here
-				// console.log('wss opened');
-			}
+interface PluginOptions {
+	name: string;
+	wss?: WebSocketServer;
+	configureServer: (pluginOptions: PluginOptions, server: ViteDevServer) => void;
+}
+
+const pluginOptions: PluginOptions = {
+	name: 'wss',
+	wss: undefined,
+	configureServer: (pluginOptions: PluginOptions, server: ViteDevServer) => {
+		console.log(pluginOptions);
+		console.log(pluginOptions?.wss ?? '123');
+		if (server.httpServer && !pluginOptions.wss) {
+			pluginOptions.wss = new WebSocketServer({
+				server: server.httpServer,
+			});
+			pluginOptions.wss.on('connection', (socket) => {
+				console.log('connected');
+				// [TODO] socket.url goes undefined
+				if (socket.url?.includes('/game')) {
+					socket.on('message', (data: unknown) => {
+						console.log(`Received message for /game endpoint: ${data}`);
+					});
+				}
+			});
+			pluginOptions.wss.on('error', (e) => {
+				console.error(e)
+			});
 		}
-	}]
+	}
+};
+
+const config: UserConfig = {
+	plugins: [
+		sveltekit(),
+		{
+			...pluginOptions,
+			configureServer: (server: ViteDevServer) =>
+				pluginOptions.configureServer(pluginOptions, server)
+		}
+	]
 };
 
 export default config;
