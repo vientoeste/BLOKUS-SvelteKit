f4b1e594678be185726aa8aa7696b9f2dea08062
este <djjd2000@naver.com>
2024-11-07 18:31:47 +0900

    Chore(BREAKING CHANGES): replace uuid to uuidv7, update svelte to 5 with dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { Server } from 'socket.io';
 
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
-		{
-			name: 'socketio',
-			configureServer(server) {
-				if (server.httpServer) {
-					const io = new Server(server.httpServer);
-					// io.on('connection', (socket) => {
-					// 	console.log('connected');
-					// })
-					const room = io.of('/room');
-					room.on('connection', (socket) => {
-						console.log('room: connected');
-						socket.on('disconnect', () => {
-							console.log('room: disconnected');
-						});
-					});
-					const game = io.of('/game');
-					game.on('connection', (socket) => {
-						console.log('game: connected');
-						// socket.on('startGame', () => {
-						// 	console.log('emitted at viteconfig')
-						// })
-						socket.on('disconnect', () => {
-							console.log('game: disconnected');
-						})
-					});
-				}
-			}
-		}
 	],
 	server: {
 		host: '0.0.0.0',
