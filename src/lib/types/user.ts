bfc0c0e1acf91994f31e9080c97f7233b89a19f0
este <djjd2000@naver.com>
2024-11-24 17:01:08 +0900

    Feature: add session, user types

---
+++ src/lib/types/user.ts
@@ -0,0 +1,25 @@
+interface UserInf {
+  id: string;
+  userId: string;
+  username: string;
+  password: string;
+}
+
+export type UserInfo = Omit<UserInf, 'password'>;
+
+export interface UserDocumentInf {
+  _id: string;
+  userId: string;
+  username: string;
+  password: string;
+  isDeleted: boolean;
+}
+
+export type CreateUserDTO = Omit<UserInf, 'id'>;
+
+export type UpdateUserDTO = Partial<Omit<UserInf, 'id'>>;
+
+export interface SignInDTO {
+  userId: string;
+  password: string;
+}
