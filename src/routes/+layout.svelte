6094129b38cad107911e8863b287fec96f437074
vientoeste <djjd2000@naver.com>
2024-11-30 15:34:59 +0900

    Feature: now login/user-info components are switched by userStore status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,27 @@
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import Login from "$lib/components/Login.svelte";
+  import Modal from "$lib/components/Modal.svelte";
+  import UserInfo from "$lib/components/UserInfo.svelte";
+
+  import { userStore } from "../Store";
   import "../app.css";
+
   let { children } = $props();
 </script>
 
 <Header />
+<Modal />
 <main class="row-layout">
   <article>
     {@render children()}
   </article>
   <aside>
-    <section id="login"><Login /></section>
+    {#if $userStore.userId === undefined}
+      <section id="login"><Login /></section>
+    {:else}
+      <section id="userInfo"><UserInfo /></section>
+    {/if}
   </aside>
 </main>
 <Footer />
