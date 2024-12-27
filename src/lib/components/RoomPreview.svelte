7cb8f7f9fc3b878f555c8eab982261e2fc2d7b3f
vientoeste <djjd2000@naver.com>
2024-12-27 18:02:04 +0900

    Feature: improve room preview component(and change prop data's type to RoomPreviewInf)

--- src/lib/components/RoomPreview.svelte
+++ src/lib/components/RoomPreview.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
-  import type { RoomInf } from "$lib/types";
-  interface $$Props extends RoomInf {}
+  import { goto } from "$app/navigation";
+  import type { RoomPreviewInf } from "$lib/types";
+  import { modalStore } from "../../Store";
+  import Alert from "./Alert.svelte";
+  interface $$Props extends RoomPreviewInf {}
 
-  const room = $props();
-  const { id, name, players } = room;
+  const room = $props() as RoomPreviewInf;
+  const { id, name, players, isStarted } = room;
 </script>
 
-<a href="rooms/{id}" class="room" data-sveltekit-preload-data>
+<a
+  href="rooms/{id}"
+  class="room row-layout"
+  onclick={(e) => {
+    e.preventDefault();
+    if (isStarted) {
+      modalStore.open(Alert, {
+        title: "game is in progress",
+        message: "try join other room",
+      });
+      return;
+    }
+    goto(`rooms/${id}`);
+  }}
+  data-sveltekit-preload-data
+>
   <div class="room-title">{name}</div>
   <div class="room-creator">{players[0].username}</div>
+  <div class="room-players">{players.length}</div>
+  <div class="room-started">
+    {isStarted === true ? "in progress" : "not in progress"}
+  </div>
 </a>
+
+<style>
+  .room {
+    padding: 10px 0;
+  }
+</style>
