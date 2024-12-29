1819306079648337142656e3876ea31b7f2a82cb
vientoeste <djjd2000@naver.com>
2024-12-29 13:51:39 +0900

    Feature: add board component

---
+++ src/lib/components/Board.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  const { board } = $props();
+</script>
+
+<div id="board">
+  {#each board as boardLine, rowIdx}
+    <div id="boardLine-{rowIdx}" class="boardLine">
+      {#each boardLine as cell, colIdx}
+        <div class="cell-cover">
+          <div id="cell-{rowIdx}-{colIdx}" class="cell cell-{cell}"></div>
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  #board {
+    width: fit-content;
+    height: fit-content;
+    background: white;
+    border: 0.5px solid black;
+  }
+  .boardLine {
+    display: flex;
+    flex-direction: row;
+  }
+  .cell-cover {
+    background: white;
+    padding: 1px;
+    border-left: 0.5px solid rgba(0, 0, 0, 0.267);
+    border-bottom: 0.5px solid rgba(0, 0, 0, 0.267);
+  }
+  .cell-cover:first-child {
+    border-left: none;
+  }
+  #boardLine-19 > .cell-cover {
+    border-bottom: none;
+  }
+  .cell {
+    width: 30px;
+    height: 30px;
+    background: rgb(185, 185, 185);
+  }
+  .cell-0 {
+    background: rgba(0, 0, 255, 0.625);
+  }
+  .cell-1 {
+    background: rgba(255, 0, 0, 0.65);
+  }
+  .cell-2 {
+    background: rgb(0, 220, 0, 0.625);
+  }
+  .cell-3 {
+    background: rgb(255, 255, 0, 0.75);
+  }
+</style>
