70b29f3230712928ee4271333c1904058f3d85fb
este <djjd2000@naver.com>
2025-01-13 01:02:48 +0900

    Feature: reflect DnD's changes to component Board

--- src/lib/components/Board.svelte
+++ src/lib/components/Board.svelte
@@ -1,13 +1,134 @@
 <script lang="ts">
-  const { board } = $props();
+  import type { BlockMatrix, BlockType } from "$lib/types";
+  import { parseJson } from "$lib/utils";
+  import {
+    draggedBlockMatrixStore,
+    dragPositionOffsetStore,
+  } from "../../Store";
+
+  const { board, relayMove } = $props();
+
+  let boardElement: HTMLElement;
+  const cellElements: HTMLElement[][] = Array(20)
+    .fill([])
+    .map(() => Array(20).fill({} as HTMLElement));
+
+  class CellHighlightManager {
+    // [TODO] implement this class for optimization:
+    // manipulate highlighted elements by sliding-window
+    // if I do this rn, it will be, must be an early-optimization
+  }
+
+  const highlightedCells: HTMLElement[] = [];
+  let lock = false;
+  function unhighlightCells() {
+    if (!lock) {
+      lock = true;
+      const length = highlightedCells.length;
+      for (let i = 0; i < length; i += 1) {
+        const cell = highlightedCells.shift();
+        cell?.classList?.remove("highlighted");
+      }
+      lock = false;
+      return;
+    }
+    unhighlightCells();
+  }
+  function highlightCells({
+    block,
+    position,
+  }: {
+    block: BlockMatrix;
+    position: number[];
+  }) {
+    unhighlightCells();
+    if (
+      position[0] > -1 &&
+      position[0] < 20 &&
+      position[1] > -1 &&
+      position[1] < 20
+    ) {
+      block.forEach((blockLine, rowIdx) => {
+        blockLine.forEach((cell, colIdx) => {
+          if (cell !== null) {
+            const currentCell =
+              cellElements[position[0] + rowIdx][position[1] + colIdx];
+            highlightedCells.push(currentCell);
+            currentCell.classList.add("highlighted");
+          }
+        });
+      });
+    }
+  }
+
+  function getPosition({ x, y }: { x: number; y: number }) {
+    const { left: boardLeft, top: boardTop } =
+      boardElement.getBoundingClientRect();
+    return [
+      Math.ceil((y + $dragPositionOffsetStore[1] - boardTop) / 33) - 1,
+      Math.ceil((x + $dragPositionOffsetStore[0] - boardLeft) / 33) - 1,
+    ];
+  }
+
+  function handleDragOver(e: DragEvent, rowIdx: number, colIdx: number) {
+    e.preventDefault();
+    // getPosition();
+    highlightCells({
+      block: $draggedBlockMatrixStore,
+      position: getPosition({ x: e.clientX, y: e.clientY }),
+    });
+  }
+
+  function handleDrop(e: DragEvent, rowIdx: number, colIdx: number) {
+    e.preventDefault();
+
+    dragPositionOffsetStore.set([0, 0]);
+    highlightedCells.forEach((e) => {
+      e.classList.remove("highlighted");
+    });
+    highlightedCells.length = 0;
+
+    try {
+      // [TODO] extend dataTrasfer's data to contain rotate, flip, ...
+      if (e.dataTransfer) {
+        const data = parseJson<{ type: BlockType }>(
+          e.dataTransfer.getData("application/json"),
+        );
+        if (typeof data === "string") {
+          return;
+        }
+
+        relayMove("blockDrop", {
+          position: getPosition({ x: e.clientX, y: e.clientY }),
+          blockType: data.type,
+        });
+
+        unhighlightCells();
+      }
+    } catch (error) {
+      console.error("Drop 처리 중 에러:", error);
+    }
+  }
 </script>
 
-<div id="board">
+<div id="board" bind:this={boardElement}>
   {#each board as boardLine, rowIdx}
     <div id="boardLine-{rowIdx}" class="boardLine">
       {#each boardLine as cell, colIdx}
         <div class="cell-cover">
-          <div id="cell-{rowIdx}-{colIdx}" class="cell cell-{cell}"></div>
+          <div
+            id="cell-{rowIdx}-{colIdx}"
+            class="cell cell-{cell}"
+            role="button"
+            tabindex="0"
+            bind:this={cellElements[rowIdx][colIdx]}
+            ondragover={(e) => {
+              handleDragOver(e, rowIdx, colIdx);
+            }}
+            ondrop={(e) => {
+              handleDrop(e, rowIdx, colIdx);
+            }}
+          ></div>
         </div>
       {/each}
     </div>
@@ -53,5 +174,9 @@
   }
   .cell-3 {
     background: rgb(255, 255, 0, 0.75);
+  }
+  .highlighted {
+    /* change to other color */
+    background: black !important;
   }
 </style>
