ff6a3a31ebb69bd8589d4b9d409c3d67b11ecdcf
este <djjd2000@naver.com>
2025-03-18 22:59:00 +0900

    Feature: add board state manager(omitted rerendering, event publishing)

--- src/lib/client/game/state/board.ts
+++ src/lib/client/game/state/board.ts
@@ -1,4 +1,5 @@
-import type { BoardMatrix } from "$types";
+import { putBlockOnBoard, rollbackMove } from "$lib/game/core";
+import type { Block, BoardMatrix, PlayerIdx, SlotIdx } from "$types";
 
 export class BoardStateManager {
   constructor({ board }: {
@@ -8,4 +9,49 @@ export class BoardStateManager {
   }
 
   private board?: BoardMatrix;
+
+  initializeBoard(board?: BoardMatrix) {
+    if (!board) {
+      this.board = Array(20).fill(null).map(() => Array(20).fill(false));
+      return;
+    }
+    this.board = board;
+  }
+
+  destroyBoard() {
+    this.board = undefined;
+    // re-render
+  }
+
+  applyNonTimeoutMove({
+    blockInfo, playerIdx, position, turn, slotIdx
+  }: {
+    blockInfo: Block,
+    playerIdx: PlayerIdx,
+    position: [number, number],
+    turn: number,
+    slotIdx: SlotIdx,
+  }) {
+    if (!this.board) return;
+    const reason = putBlockOnBoard({
+      blockInfo,
+      board: this.board,
+      playerIdx,
+      position,
+      slotIdx,
+      turn,
+    });
+    if (reason) {
+      // emit event
+      rollbackMove({
+        blockInfo,
+        board: this.board,
+        position,
+        slotIdx,
+      });
+      return;
+    }
+    // emit event
+    // re-render here
+  }
 }
