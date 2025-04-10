ce468547f9657648f2ae81b71b9776fc22633f89
vientoeste <djjd2000@naver.com>
2025-04-10 12:45:06 +0900

    Style: typo

--- src/lib/game/core.ts
+++ src/lib/game/core.ts
@@ -338,10 +338,10 @@ export const hasValidPlacement = ({ board, slotIdx, blockType }: {
 
 // [TODO] add last-monomino-rule
 export const calculatePlayerScores = (board: string): Record<SlotIdx, number> => {
-  const BoardMatrix = convertBoardToArr(board);
+  const boardMatrix = convertBoardToArr(board);
   const scores: Record<SlotIdx, number> = { 0: 0, 1: 0, 2: 0, 3: 0 };
 
-  BoardMatrix.forEach(row => {
+  boardMatrix.forEach(row => {
     row.forEach(cell => {
       if (cell !== false) {
         scores[cell as SlotIdx] += 1;
