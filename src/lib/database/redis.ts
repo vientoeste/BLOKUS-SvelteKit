afaeb6119de66a22afa1eb1a913159300297de35
este <djjd2000@naver.com>
2025-04-12 00:15:37 +0900

    Feature: add repository, schema for roomCache, gameEndSequenceCache

--- src/lib/database/redis.ts
+++ src/lib/database/redis.ts
@@ -48,3 +48,36 @@ export const redisModelFactory = {
     };
   },
 };
+
+export const { repository: roomCacheRepository, schema: roomCacheSchema } = redisModelFactory.create('room', {
+  id: { type: 'string' as const },
+  name: { type: 'string' as const },
+  gameId: { type: 'string' as const },
+  turn: { type: 'number' as const },
+  lastMove: { type: 'string' as const },
+  started: { type: 'boolean' as const },
+  p0_id: { type: 'string' as const },
+  p0_username: { type: 'string' as const },
+  p0_ready: { type: 'boolean' as const },
+  p0_exhausted: { type: 'boolean' as const },
+  p1_id: { type: 'string' as const },
+  p1_username: { type: 'string' as const },
+  p1_ready: { type: 'boolean' as const },
+  p1_exhausted: { type: 'boolean' as const },
+  p2_id: { type: 'string' as const },
+  p2_username: { type: 'string' as const },
+  p2_ready: { type: 'boolean' as const },
+  p2_exhausted: { type: 'boolean' as const },
+  p3_id: { type: 'string' as const },
+  p3_username: { type: 'string' as const },
+  p3_ready: { type: 'boolean' as const },
+  p3_exhausted: { type: 'boolean' as const },
+});
+
+export const { repository: gameEndSequenceRepository, schema: gameEndSequenceSchema } = redisModelFactory.create('game_end_sequence', {
+  finalBoardState: { type: 'string' as const },
+  initiatedBy: { type: 'number' as const },
+  endDate: { type: 'date' as const },
+  finalScore: { type: 'number' as const },
+  confirmationStatus: { type: 'number[]' as const },
+});
