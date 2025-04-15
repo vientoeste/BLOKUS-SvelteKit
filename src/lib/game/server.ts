9c748373b2c02a4eab2354fa547327cf6d7ab44e
vientoeste <djjd2000@naver.com>
2025-04-16 00:30:25 +0900

    Feature: add generateGameSummary to calculate score on server

---
+++ src/lib/game/server.ts
@@ -0,0 +1,22 @@
+import { getMovesByGameId } from "$lib/database/move";
+import { Score } from "$lib/domain/score";
+import type { GameId, RegularMove } from "$types";
+import { createNewBoard, getBlockMatrix, placeBlock } from "./core";
+
+export const generateGameSummary = async (gameId: GameId): Promise<Score> => {
+  const moves = await getMovesByGameId(gameId, {
+    onlyRegularMoves: true,
+  }) as RegularMove[];
+  const board = createNewBoard();
+  moves.forEach((move) => {
+    placeBlock({
+      block: getBlockMatrix(move.blockInfo),
+      board,
+      position: move.position,
+      slotIdx: move.slotIdx,
+      turn: move.turn,
+    });
+  });
+  // [TODO] add last-monomino-rule
+  return Score.fromBoard(board);
+};
