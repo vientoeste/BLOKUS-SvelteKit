ca4479f9f0a2878f5208d92e7fc36987968293eb
este <djjd2000@naver.com>
2025-07-08 17:19:29 +0900

    F/R: update type of event from enum to 'as const' and add some of network layer's events

--- src/lib/types/client/event.ts
+++ src/lib/types/client/event.ts
@@ -1,6 +1,22 @@
-export enum EventType {
+export const Event = {
+  MessageReceived_CancelReady: 'MessageReceived_CancelReady',
+  MessageReceived_Connected: 'MessageReceived_Connected',
+  MessageReceived_Leave: 'MessageReceived_Leave',
+  MessageReceived_Move: 'MessageReceived_Move',
+  MessageReceived_Ready: 'MessageReceived_Ready',
+  MessageReceived_Mediate: 'MessageReceived_Mediate',
+  MessageReceived_Error: 'MessageReceived_Error',
+  MessageReceived_Start: 'MessageReceived_Start',
+  MessageReceived_BadReq: 'MessageReceived_BadReq',
+  MessageReceived_Exhausted: 'MessageReceived_Exhausted',
+  MessageReceived_SkipTurn: 'MessageReceived_SkipTurn',
+  MessageReceived_ScoreConfirmation: 'MessageReceived_ScoreConfirmation',
+  MessageReceived_GameEnd: 'MessageReceived_GameEnd',
 
-}
+  DispatchMessage: 'DispatchMessage',
+} as const;
+
+export type EventType = typeof Event;
 
 // [TODO] define payload
 export interface GameEvent {
