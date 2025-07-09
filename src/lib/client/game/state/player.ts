fb0c07bd48a03ae3541d9564192d15974cfa34a4
este <djjd2000@naver.com>
2025-07-09 16:57:10 +0900

    Feature: implement player state manager to perfectly interact with new store

--- src/lib/client/game/state/player.ts
+++ src/lib/client/game/state/player.ts
@@ -1,35 +1,85 @@
-import type { ParticipantInf, PlayerIdx } from "$types";
+import { participantStore } from "$lib/store";
+import type { ParticipantInf, PlayerId, PlayerIdx } from "$types";
+import { get } from "svelte/store";
 import type { EventBus } from "../event";
 
 // [TODO] refactor to use event bus
 export class PlayerStateManager {
   constructor({ players, eventBus }: { players: (ParticipantInf | undefined)[], eventBus: EventBus }) {
-    this.players = players;
     this.eventBus = eventBus;
+    participantStore.initialize(players);
+
+    this.eventBus.subscribe('MessageReceived_CancelReady', (event) => {
+      // [TODO] add payload type and replace 'as' statement
+      const { playerIdx } = event.payload as { playerIdx: PlayerIdx };
+      if (playerIdx === undefined) return;
+      this.updateReadyState({
+        playerIdx,
+        ready: false,
+      });
+    });
+    this.eventBus.subscribe('MessageReceived_Connected', (event) => {
+      // [TODO] add payload type and replace 'as' statement
+      const { id, username, playerIdx } = event.payload as {
+        id: PlayerId,
+        username: string,
+        playerIdx: PlayerIdx,
+      };
+      if (!playerIdx) return;
+      this.addPlayer({
+        id,
+        username,
+        playerIdx,
+        ready: false,
+      });
+    });
+    this.eventBus.subscribe('MessageReceived_Ready', (event) => {
+      // [TODO] add payload type and replace 'as' statement
+      const { playerIdx } = event.payload as {
+        playerIdx: PlayerIdx,
+      };
+      if (playerIdx === undefined) return;
+      this.updateReadyState({
+        playerIdx,
+        ready: true,
+      });
+    });
+    this.eventBus.subscribe('MessageReceived_Leave', (event) => {
+      // [TODO] add payload type and replace 'as' statement
+      const { playerIdx } = event.payload as {
+        playerIdx: PlayerIdx,
+      };
+      if (!playerIdx) return;
+      this.removePlayerByIdx(playerIdx);
+    });
   }
 
   private eventBus: EventBus;
-  private players: (ParticipantInf | undefined)[] = [undefined, undefined, undefined, undefined];
 
   getPlayers() {
-    return [...this.players];
+    return get(participantStore);
   }
 
   addPlayer({ id, playerIdx, username }: ParticipantInf & { playerIdx: PlayerIdx }) {
-    this.players[playerIdx] = {
-      id,
-      ready: false,
-      username,
-    };
+    participantStore.addPlayer({
+      id, playerIdx, username, ready: false,
+    });
   }
 
   removePlayerByIdx(playerIdx: PlayerIdx) {
-    this.players[playerIdx] = undefined;
+    participantStore.removePlayerByIdx(playerIdx);
   }
 
   updateReadyState({ playerIdx, ready }: { playerIdx: PlayerIdx, ready: boolean }) {
-    if (this.players[playerIdx]) {
-      this.players[playerIdx].ready = ready;
-    }
+    participantStore.setPlayerReadyState({
+      playerIdx,
+      ready,
+    });
+  }
+
+  getActivePlayerCount() {
+    const players = this.getPlayers();
+    if (players === undefined) return 0;
+    return players.filter(e => e !== undefined).length;
   }
 }
