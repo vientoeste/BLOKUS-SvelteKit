aaf52639dd91e34897c4bd6bdbf86ea419b304e5
este <djjd2000@naver.com>
2025-07-16 16:28:09 +0900

    Feature: add constructor of MoveStateManager

--- src/lib/client/game/state/move.ts
+++ src/lib/client/game/state/move.ts
@@ -1,3 +1,14 @@
+import type { Move } from "$types";
+import type { EventBus } from "../event";
+
 export class MoveStateManager {
-  
+  constructor({ eventBus, moves }: { eventBus: EventBus, moves?: Move[] }) {
+    this.eventBus = eventBus;
+    if (moves !== undefined) {
+      this.moveHistory.push(...moves);
+    }
+  }
+
+  private eventBus: EventBus;
+  private moveHistory: Move[] = [];
 }
