a7cda29979e2c0ce5476d2a2f3d65de9c51950ab
este <djjd2000@naver.com>
2025-07-16 14:57:01 +0900

    Feature: add BlockStateManager.removeBlockFromStore

---
+++ src/lib/client/game/state/block.ts
@@ -0,0 +1,20 @@
+import { blockStore } from "$lib/store";
+import type { BlockType, SlotIdx } from "$types";
+import type { EventBus } from "../event";
+
+export class BlockStateManager {
+  constructor({ eventBus }: { eventBus: EventBus }) {
+    this.eventBus = eventBus;
+
+    this.eventBus.subscribe('MoveApplied', (event) => {
+      const { slotIdx, blockInfo: { type } } = event.payload;
+      this.removeBlockFromStore({ blockType: type, slotIdx });
+    });
+  }
+
+  private eventBus: EventBus;
+
+  removeBlockFromStore({ blockType, slotIdx }: { blockType: BlockType, slotIdx: SlotIdx }) {
+    blockStore.updateBlockPlacementStatus({ blockType, slotIdx });
+  }
+}
