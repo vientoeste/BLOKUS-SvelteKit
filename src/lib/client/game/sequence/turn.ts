2db43131d07ff9c62872e7237a80af16ff1aa6be
este <djjd2000@naver.com>
2025-07-20 18:13:55 +0900

    Refac: rewrite utils' isRightTurn to TurnSequencer.calculatePlayerIdx

--- src/lib/client/game/sequence/turn.ts
+++ src/lib/client/game/sequence/turn.ts
@@ -6,4 +6,28 @@ export class TurnSequencer {
   }
 
   private eventBus: EventBus;
+
+  private calculatePlayerIdx({
+    turn,
+    activePlayerCount,
+  }: {
+    turn: number;
+    activePlayerCount: 2 | 3 | 4;
+  }) {
+    if (activePlayerCount === 2) {
+      return turn % 2;
+    }
+    /**
+     * @description tmp variable to reduce duplicated calculation
+     */
+    const turnModular4 = turn % 4;
+    if (activePlayerCount === 3) {
+      return turnModular4 === 3 ?
+        (turn % 12 - 3) / 4 :
+        turnModular4;
+    }
+    if (activePlayerCount === 4) {
+      return turnModular4;
+    }
+  }
 }
