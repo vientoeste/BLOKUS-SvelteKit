b88bb94b61d7eb7e8021a057ac7de7978df1e333
este <djjd2000@naver.com>
2025-08-05 12:48:03 +0900

    Refac: remove eventBus injection of BlockStateManager

--- src/lib/client/game/state/block.ts
+++ src/lib/client/game/state/block.ts
@@ -1,14 +1,7 @@
 import { blockStore } from "$lib/store";
 import type { BlockType, SlotIdx } from "$types";
-import type { EventBus } from "../event";
 
 export class BlockStateManager {
-  constructor({ eventBus }: { eventBus: EventBus }) {
-    this.eventBus = eventBus;
-  }
-
-  private eventBus: EventBus;
-
   initialize(slots: SlotIdx[]) {
     blockStore.initialize(slots);
   }
