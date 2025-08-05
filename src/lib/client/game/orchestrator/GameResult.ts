9f1e59c34cb3d0485dd8ec600a2f8a58c1da9047
este <djjd2000@naver.com>
2025-08-05 19:28:39 +0900

    Feature: inject GameStateManager, BoardStateManager to GameResultOrchestrator

--- src/lib/client/game/orchestrator/GameResult.ts
+++ src/lib/client/game/orchestrator/GameResult.ts
@@ -1,14 +1,26 @@
+import { Score } from "$lib/domain/score";
+import type { InboundScoreConfirmationMessage } from "$types";
 import type { EventBus } from "../event";
+import type { BoardStateManager } from "../state/board";
+import type { GameStateManager } from "../state/game";
 
 export class GameResultOrchestrator {
   private eventBus: EventBus;
+  private gameStateManager: GameStateManager;
+  private boardStateManager: BoardStateManager;
 
   constructor({
     eventBus,
+    gameStateManager,
+    boardStateManager,
   }: {
     eventBus: EventBus;
+    gameStateManager: GameStateManager;
+    boardStateManager: BoardStateManager;
   }) {
     this.eventBus = eventBus;
+    this.gameStateManager = gameStateManager;
+    this.boardStateManager = boardStateManager
 
     this.eventBus.subscribe('MessageReceived_ScoreConfirmation', (event) => {
       // [TODO] mark game as ended
