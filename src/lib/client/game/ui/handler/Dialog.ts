1064fa6a2a0ba0cdb012e6437f26bab11a30388e
este <djjd2000@naver.com>
2025-08-07 18:17:18 +0900

    Feature: add ConfirmManager._open

--- src/lib/client/game/ui/handler/Dialog.ts
+++ src/lib/client/game/ui/handler/Dialog.ts
@@ -1,5 +1,30 @@
-export class ConfirmManager {
+import Confirm from "$lib/components/Confirm.svelte";
+import { modalStore } from "$lib/store";
 
+type ConfirmResult = 'CONFIRM' | 'CANCEL' | 'CLOSE';
+
+export class ConfirmManager {
+  private _open({
+    title,
+    message,
+    confirmText = 'confirm',
+    cancelText = 'cancel',
+  }: {
+    title: string;
+    message: string;
+    confirmText?: string;
+    cancelText?: string;
+  }): Promise<ConfirmResult> {
+    return new Promise<ConfirmResult>((res) => modalStore.open(Confirm, {
+      title,
+      message,
+      confirmText,
+      cancelText,
+      onConfirm: () => res('CONFIRM'),
+      onClose: () => res('CLOSE'),
+      onCancel: () => res('CANCEL'),
+    }));
+  }
 }
 
 export class AlertManager {
