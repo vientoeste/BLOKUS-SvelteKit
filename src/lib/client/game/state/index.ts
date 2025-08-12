5e64e6f17e5da3085a68bf3b2afac99297740e2a
este <djjd2000@naver.com>
2025-08-12 15:54:09 +0900

    Feature: inject missing state classes to layer class

--- src/lib/client/game/state/index.ts
+++ src/lib/client/game/state/index.ts
@@ -1,19 +1,38 @@
+import type { BlockStateManager } from "./block";
 import type { BoardStateManager } from "./board";
+import type { GameStateManager } from "./game";
 import type { MoveStateManager } from "./move";
 import type { PlayerStateManager } from "./player";
+import type { SlotStateManager } from "./slot";
 
 export class GameStateLayer {
-  constructor({ playerStateManager, boardStateManager, moveStateManager }: {
-    playerStateManager: PlayerStateManager;
+  constructor({
+    blockStateManager,
+    boardStateManager,
+    gameStateManager,
+    moveStateManager,
+    playerStateManager,
+    slotStateManager,
+  }: {
+    blockStateManager: BlockStateManager;
     boardStateManager: BoardStateManager;
+    gameStateManager: GameStateManager;
     moveStateManager: MoveStateManager;
+    slotStateManager: SlotStateManager;
+    playerStateManager: PlayerStateManager;
   }) {
-    this.playerStateManager = playerStateManager;
+    this.blockStateManager = blockStateManager;
     this.boardStateManager = boardStateManager;
+    this.gameStateManager = gameStateManager;
     this.moveStateManager = moveStateManager;
+    this.playerStateManager = playerStateManager;
+    this.slotStateManager = slotStateManager;
   }
 
-  private playerStateManager: PlayerStateManager;
+  private blockStateManager: BlockStateManager;
   private boardStateManager: BoardStateManager;
+  private gameStateManager: GameStateManager;
   private moveStateManager: MoveStateManager;
+  private playerStateManager: PlayerStateManager;
+  private slotStateManager: SlotStateManager;
 }
