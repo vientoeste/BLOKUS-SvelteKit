c38cfe2133736ab0e600bcb97504c3838f34cea0
este <djjd2000@naver.com>
2025-08-12 15:57:50 +0900

    Feature: inject TurnSequencer, PlayerTurnTimer to GameSequenceLayer

--- src/lib/client/game/sequence/index.ts
+++ src/lib/client/game/sequence/index.ts
@@ -1,4 +1,18 @@
+import type { PlayerTurnTimer } from "./timer";
+import type { TurnSequencer } from "./turn";
+
 // [TODO] sequence layer? play layer?
 export class GameSequenceLayer {
+  private turnSequencer: TurnSequencer;
+  private turnTimer: PlayerTurnTimer;
 
+  constructor({
+    turnSequencer, turnTimer,
+  }: {
+    turnSequencer: TurnSequencer;
+    turnTimer: PlayerTurnTimer;
+  }) {
+    this.turnSequencer = turnSequencer;
+    this.turnTimer = turnTimer;
+  }
 }
