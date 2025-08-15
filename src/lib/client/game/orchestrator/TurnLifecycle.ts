b31846c7ef77417248d48fa1ae8c8a346369c8c5
este <djjd2000@naver.com>
2025-08-16 03:25:19 +0900

    Refac: apply necessary ports to TurnLifecycleOrch

--- src/lib/client/game/orchestrator/TurnLifecycle.ts
+++ src/lib/client/game/orchestrator/TurnLifecycle.ts
@@ -1,13 +1,14 @@
 import type { OutboundMoveMessage, OutboundSkipTurnMessage, PlayerIdx } from "$types";
 import type { BlockPlaceabilityCalculator } from "../domain/blockPlaceabilityCalculator";
 import type { EventBus } from "../event";
-import type { BlockStateManager } from "../state/block";
-import type { BoardStateManager } from "../state/board";
-import type { GameStateManager } from "../state/game";
-import type { MoveStateManager } from "../state/move";
-import type { PlayerStateManager } from "../state/player";
-import type { SlotStateManager } from "../state/slot";
-
+import type {
+  ITurnManager,
+  ICalculationDataProvider,
+  ICalculationResultApplier,
+  IMoveApplier,
+  ISlotManager,
+  IClientInfoReader
+} from '../application/ports';
 
 // [TODO] Refactor TurnLifecycleOrchestrator responsibilities:
 /*
@@ -34,48 +35,48 @@ import type { SlotStateManager } from "../state/slot";
  */
 export class TurnLifecycleOrchestrator {
   private eventBus: EventBus;
-  private boardStateManager: BoardStateManager;
-  private blockStateManager: BlockStateManager;
   private blockPlaceabilityCalculator: BlockPlaceabilityCalculator;
-  private playerStateManager: PlayerStateManager;
-  private slotStateManager: SlotStateManager;
-  private gameStateManager: GameStateManager;
-  private moveStateManager: MoveStateManager;
+  private turnManager: ITurnManager;
+  private calculationDataProvider: ICalculationDataProvider;
+  private calculationResultApplier: ICalculationResultApplier;
+  private moveApplier: IMoveApplier;
+  private slotManager: ISlotManager;
+  private clientInfoReader: IClientInfoReader;
 
   constructor({
     eventBus,
-    boardStateManager,
-    blockStateManager,
     blockPlaceabilityCalculator,
-    playerStateManager,
-    slotStateManager,
-    gameStateManager,
-    moveStateManager,
+    turnManager,
+    calculationDataProvider,
+    calculationResultApplier,
+    moveApplier,
+    slotManager,
+    clientInfoReader,
   }: {
     eventBus: EventBus;
-    boardStateManager: BoardStateManager;
-    blockStateManager: BlockStateManager;
     blockPlaceabilityCalculator: BlockPlaceabilityCalculator;
-    playerStateManager: PlayerStateManager;
-    slotStateManager: SlotStateManager;
-    gameStateManager: GameStateManager;
-    moveStateManager: MoveStateManager;
+    turnManager: ITurnManager;
+    calculationDataProvider: ICalculationDataProvider;
+    calculationResultApplier: ICalculationResultApplier;
+    moveApplier: IMoveApplier;
+    slotManager: ISlotManager;
+    clientInfoReader: IClientInfoReader;
   }) {
     this.eventBus = eventBus;
-    this.boardStateManager = boardStateManager;
-    this.blockStateManager = blockStateManager;
     this.blockPlaceabilityCalculator = blockPlaceabilityCalculator;
-    this.playerStateManager = playerStateManager;
-    this.slotStateManager = slotStateManager;
-    this.gameStateManager = gameStateManager;
-    this.moveStateManager = moveStateManager;
+    this.turnManager = turnManager
+    this.calculationDataProvider = calculationDataProvider
+    this.calculationResultApplier = calculationResultApplier
+    this.moveApplier = moveApplier
+    this.slotManager = slotManager
+    this.clientInfoReader = clientInfoReader
 
     this.eventBus.subscribe('GameStateInitialized', () => {
-      if (this.gameStateManager.getCurrentTurn() === 0) {
+      if (this.turnManager.getCurrentTurn() === 0) {
         this.eventBus.publish('TurnAdvanced', {
           turn: 0,
-          activePlayerCount: this.gameStateManager.getActivePlayerCount(),
-          playerIdx: this.playerStateManager.getClientPlayerIdx(),
+          activePlayerCount: this.turnManager.getActivePlayerCount(),
+          playerIdx: this.clientInfoReader.getClientPlayerIdx(),
         });
       }
     });
@@ -96,12 +97,12 @@ export class TurnLifecycleOrchestrator {
      */
     this.eventBus.subscribe('MessageReceived_Exhausted', (event) => {
       const { slotIdx } = event.payload;
-      this.slotStateManager.applyExhaustedState(slotIdx);
+      this.slotManager.applyExhaustedState(slotIdx);
     });
   }
 
   private verifyMoveContext({ turn }: { turn: number }) {
-    const result = this.gameStateManager.verifyMoveContext({ turn });
+    const result = this.turnManager.verifyMoveContext({ turn });
     if (!result.isValid) {
       switch (result.reason) {
         case 'game is not started':
@@ -125,16 +126,13 @@ export class TurnLifecycleOrchestrator {
     if (!gameId) {
       return;
     }
-    const { result, reason } = this.boardStateManager.checkBlockPleaceability(move);
+    const { result, reason } = this.moveApplier.checkBlockPlaceability(move);
     if (!result) {
       // [TODO] add events for mediate / error report / ... using `reason`
       this.eventBus.publish('BlockNotPlaceable', { reason });
       return;
     }
-    this.boardStateManager.placeBlock(move);
-    // [TODO] createdAt should be replaced as server-sent timestamp
-    this.moveStateManager.addMoveToHistory({ ...move, gameId, createdAt: new Date(), timeout: false, exhausted: false });
-    this.blockStateManager.removeBlockFromStore({ blockType: move.blockInfo.type, slotIdx: move.slotIdx });
+    this.moveApplier.applyRegularMove(move);
   }
 
   private handleSkipMessage(skipMove: OutboundSkipTurnMessage) {
@@ -142,24 +140,23 @@ export class TurnLifecycleOrchestrator {
     if (!gameId) {
       return;
     }
-    // [TODO] createdAt should be replaced as server-sent timestamp
-    this.moveStateManager.addMoveToHistory({ ...skipMove, gameId, createdAt: new Date() });
+    this.moveApplier.applySkipMove(skipMove);
   }
 
   private async finalizeTurn({ playerIdx }: { playerIdx: PlayerIdx }) {
     // 1. advance turn
-    const nextTurn = this.gameStateManager.advanceTurn();
+    const nextTurn = this.turnManager.advanceTurn();
     if (nextTurn !== -1) {
       this.eventBus.publish('TurnAdvanced', {
         turn: nextTurn,
-        activePlayerCount: this.gameStateManager.getActivePlayerCount(),
+        activePlayerCount: this.turnManager.getActivePlayerCount(),
         playerIdx,
       });
     }
 
     // 2. calculate placeability of remaining blocks
-    const unusedBlocks = this.blockStateManager.getUnusedBlocks();
-    const board = this.boardStateManager.getBoard();
+    const unusedBlocks = this.calculationDataProvider.getUnusedBlocks();
+    const board = this.calculationDataProvider.getBoard();
     if (board === undefined) return;
     const result = await this.blockPlaceabilityCalculator.calculate({
       unusedBlocks,
@@ -168,11 +165,11 @@ export class TurnLifecycleOrchestrator {
 
     if (typeof result !== 'boolean' && result !== undefined) {
       // 3. apply [2]'s result to remaining blocks(available <-> unavailable)
-      this.blockStateManager.updateAvailability(result.unavailable);
+      this.calculationResultApplier.updateBlockAvailability(result.unavailable);
 
       // [TODO] reduce calculations
       // 4. check exhausted slots of the player
-      const clientSlots = this.playerStateManager.getClientSlots();
+      const clientSlots = this.clientInfoReader.getClientSlots();
       const unavailableSlots = clientSlots.filter(sIdx =>
         !result.available.some(block => block.slotIdx === sIdx)
       );
