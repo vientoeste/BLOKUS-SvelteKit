2217ad416bf275200f52fbec5ee4fda08f4674ea
este <djjd2000@naver.com>
2025-08-16 18:20:38 +0900

    Refac: remove dependencies of GameManager, especially all layer classes(to make GameManager facade)

--- src/lib/client/game/index.ts
+++ src/lib/client/game/index.ts
@@ -1,36 +1,16 @@
-import type { Block } from "$types";
-import type { EventBus } from "./event";
-import type { NetworkLayer } from "./network";
-import type { GameSequenceLayer } from "./sequence";
-import type { GameStateLayer } from "./state";
-import type { UILayer } from "./ui";
+import type { Block, SlotIdx } from "$types";
+import { EventBus } from "./event";
 
 export class GameManager {
+  private eventBus: EventBus;
+
   constructor({
     eventBus,
-    uiLayer,
-    sequenceLayer,
-    networkLayer,
-    stateLayer,
   }: {
     eventBus: EventBus;
-    uiLayer: UILayer;
-    sequenceLayer: GameSequenceLayer;
-    networkLayer: NetworkLayer;
-    stateLayer: GameStateLayer;
   }) {
     this.eventBus = eventBus;
-    this.uiLayer = uiLayer;
-    this.sequenceLayer = sequenceLayer;
-    this.networkLayer = networkLayer;
-    this.stateLayer = stateLayer;
   }
-
-  private eventBus: EventBus;
-  private uiLayer: UILayer;
-  private sequenceLayer: GameSequenceLayer;
-  private networkLayer: NetworkLayer;
-  private stateLayer: GameStateLayer;
 
   submitMove({
     previewUrl,
