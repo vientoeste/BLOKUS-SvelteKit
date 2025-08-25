36a90ff5e3bb5ef11e6e27d566a95e227823dba4
vientoeste <djjd2000@naver.com>
2025-08-25 14:00:52 +0900

    F/R: add lastMoveTimestamp to payload of event TurnProgressionTriggered (which is triggered by event GameRestoreRequested) and update GameRestoreRequest subscriber callback

--- src/lib/types/client/event/payload.ts
+++ src/lib/types/client/event/payload.ts
@@ -6,6 +6,8 @@ import type { GameId, PlayerIdx } from "$types/room";
 export type TurnProgressionTriggeredPayload = {
   turn: number;
   activePlayerCount: 2 | 3 | 4;
+  // This field is only for reconnected
+  lastMoveTimestamp?: Date;
 }
 
 export type SlotExhaustedPayload = {
