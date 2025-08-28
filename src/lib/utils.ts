a2109bef0261b3e07f073ec62a5fe49bc3a044ee
vientoeste <djjd2000@naver.com>
2025-08-29 03:12:57 +0900

    Refac: improve move compression logics

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -230,6 +230,9 @@ export const convertBoardToArr = (board: string): BoardMatrix =>
     board.slice(i * 20, i * 20 + 20).split('').map(cell => (cell === '4' ? false : parseInt(cell)))
   );
 
+type Flip = 'f' | '';
+export type CompressedMove = `${PlayerIdx}:${BlockType}:${number}:${number}:${Rotation}:${Flip}`;
+
 export const compressMove = ({
   playerIdx,
   type,
@@ -242,15 +245,12 @@ export const compressMove = ({
   position: [number, number],
   rotation: Rotation,
   flip: boolean,
-}) =>
-  `${playerIdx}:${type}[${position[0]},${position[1]}]r${rotation}${flip ? 'f' : ''}`;
+}): CompressedMove =>
+  `${playerIdx}:${type}:${position[0]}:${position[1]}:${rotation}:${flip ? 'f' : ''}`;
 
-export const decompressMove = (compressedMove: string) => {
-  const [playerIdx, ...rest1] = compressedMove.split(':');
-  const [type, ...rest2] = rest1[0].split('[');
-  const [position0, ...rest3] = rest2[0].split(',');
-  const [position1, ...rest4] = rest3[0].split(']');
-  const [, rotation, ...flip] = rest4[0].split('');
+export const decompressMove = (compressedMove: CompressedMove) => {
+  const [playerIdx, type, position0, position1, rotation, flip] = compressedMove.split(':');
+
   return {
     playerIdx: parseInt(playerIdx) as PlayerIdx,
     type: type as BlockType,
