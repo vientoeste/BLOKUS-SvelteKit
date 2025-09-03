0fa61a64e79f1a723234774c2d5918e11ae865e3
este <djjd2000@naver.com>
2025-09-04 08:09:13 +0900

    Feature: add component Cell for board and block

---
+++ src/lib/components/game/Cell.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  const { color } = $props<{
+    color: "none" | "blue" | "red" | "green" | "yellow" | "spacing";
+  }>();
+</script>
+
+{#if color !== "spacing"}
+  <div class="cell-cover">
+    <div class="cell-blank">
+      <div class="cell-content" style="background-color: var(--{color});">
+        &nbsp;
+      </div>
+    </div>
+  </div>
+{:else}
+  <div class="spacing-cell">&nbsp;</div>
+{/if}
+
+<style>
+  .cell-cover {
+    background-color: var(--none);
+    padding: 1px;
+    pointer-events: auto;
+  }
+
+  .cell-blank {
+    background-color: #fff;
+    padding: 1px;
+  }
+
+  .cell-content {
+    height: var(--block-size);
+    width: var(--block-size);
+  }
+
+  .spacing-cell {
+    height: calc(var(--block-size) + 4px);
+    width: calc(var(--block-size) + 4px);
+    opacity: 0%;
+    pointer-events: none;
+  }
+</style>
