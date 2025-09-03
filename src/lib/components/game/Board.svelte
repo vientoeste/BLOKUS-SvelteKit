d98b240fb13ad880e930e34abdee7f440898e22a
este <djjd2000@naver.com>
2025-09-04 08:11:15 +0900

    Feature: add component board (only ui implemented - DnD features must be added)

---
+++ src/lib/components/game/Board.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { boardStore } from "$lib/store";
+  import RowContainer from "./RowContainer.svelte";
+</script>
+
+<div id="board">
+  {#each $boardStore as boardRow}
+    <div class="board__row">
+      <RowContainer columns={boardRow}></RowContainer>
+    </div>
+  {/each}
+</div>
+
+<style>
+  #board {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .board__row + .board__row {
+    margin-top: -1px;
+  }
+</style>
