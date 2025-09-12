4b5469cf4a7644dd889ec6db966b73599161f38d
este <djjd2000@naver.com>
2025-09-12 16:11:42 +0900

    Feature: add skeleton of game page

---
+++ src/routes/v2/rooms/[room_id]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Board from "$lib/components/game/Board.svelte";
+  import TriplePanelLayout from "$lib/components/TriplePanelLayout.svelte";
+</script>
+
+<TriplePanelLayout>
+  <Board />
+
+  {#snippet left()}
+    <div id="left-container"></div>
+  {/snippet}
+
+  {#snippet right()}
+    <div id="right-container"></div>
+  {/snippet}
+</TriplePanelLayout>
+
+<style>
+  #left-container,
+  #right-container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
