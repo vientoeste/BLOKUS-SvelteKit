8086a9434af390f5063ec900de75d19991ea9ff7
vientoeste <djjd2000@naver.com>
2025-09-12 23:57:12 +0900

    Feature: add new Blocks component

---
+++ src/lib/components/game/Block.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { getBlockMatrix } from "$lib/game/core";
+  import ColorMatrixRenderer from "./ColorMatrixRenderer.svelte";
+
+  const { blockType, slotIdx, rotation, flip } = $props();
+
+  const blockMatrix = getBlockMatrix({
+    type: blockType,
+    flip,
+    rotation,
+  }).map((row) =>
+    row.map((cell) => {
+      if (cell) {
+        return slotIdx;
+      }
+      return -1;
+    }),
+  );
+</script>
+
+<div class="block">
+  <ColorMatrixRenderer id={`block-${blockType}`} matrix={blockMatrix}
+  ></ColorMatrixRenderer>
+</div>
