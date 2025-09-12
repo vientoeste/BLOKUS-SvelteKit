b000ff06e4619ea64795eac8b270acc2cfdbebcd
este <djjd2000@naver.com>
2025-09-12 15:39:09 +0900

    Feature: add layout v2

--- src/routes/v2/+layout.svelte
+++ src/routes/v2/+layout.svelte
@@ -1,2 +1,15 @@
 <script lang="ts">
+  import { innerHeightStore } from "$lib/store";
+  import type { Snippet } from "svelte";
+  import "../../app.css";
+
+  let {
+    children,
+  }: {
+    children: Snippet;
+  } = $props();
 </script>
+
+<svelte:window bind:innerHeight={$innerHeightStore} />
+
+{@render children()}
