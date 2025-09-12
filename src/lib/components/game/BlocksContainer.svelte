6d8d401b7f258ed5139e54759482abe82edb5843
vientoeste <djjd2000@naver.com>
2025-09-13 00:00:30 +0900

    Feature: add component BlocksContainer

---
+++ src/lib/components/game/BlocksContainer.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { unusedBlockStore } from "$lib/store";
+  import Block from "./Block.svelte";
+
+  let rerenderer = $state(0);
+</script>
+
+<div id="blocks-container">
+  {#key rerenderer}
+    {#each $unusedBlockStore as block (block.slotIdx + block.blockType)}
+      <Block
+        blockType={block.blockType}
+        slotIdx={block.slotIdx}
+        rotation={block.rotation}
+        flip={block.flip}
+      />
+    {/each}
+  {/key}
+</div>
+
+<style>
+  #blocks-container {
+    width: 300px;
+    min-width: 296px;
+    height: 100%;
+    overflow-y: scroll;
+    gap: 5px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
