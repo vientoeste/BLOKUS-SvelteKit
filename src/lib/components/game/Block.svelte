8f0f2f70649e1893f701fae3ce19109f9183786a
vientoeste <djjd2000@naver.com>
2025-09-15 13:55:33 +0900

    Feature: add drag functions to component Block

--- src/lib/components/game/Block.svelte
+++ src/lib/components/game/Block.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-  // [TODO] DnD should be added
   import { getBlockMatrix } from "$lib/game/core";
+  import {
+    blockSizeStore,
+    dragPositionOffsetStore,
+    modalStore,
+    moveStore,
+  } from "$lib/store";
+  import Alert from "../Alert.svelte";
   import ColorMatrixRenderer from "./ColorMatrixRenderer.svelte";
 
   const { blockType, slotIdx, rotation, flip, placeable } = $props();
@@ -17,9 +23,66 @@
       return -1;
     }),
   );
+
+  const handleDragStart = (event: DragEvent) => {
+    if (!placeable) {
+      modalStore.open(Alert, {
+        title: "chosen block is not available",
+        content: "please try other block",
+      });
+      event.stopPropagation();
+      return;
+    }
+    if (!event.dataTransfer) {
+      console.error(
+        "Uncaught exception: handleDragStart's event.dataTransfer is null",
+      );
+      event.stopPropagation();
+      return;
+    }
+
+    moveStore.set({
+      type: blockType,
+      flip,
+      rotation,
+      slotIdx,
+    });
+
+    event.dataTransfer.effectAllowed = "move";
+
+    const element = event.target as HTMLElement;
+    element.style.opacity = "0.4";
+
+    const blockElement = event.target;
+    if (blockElement === null) {
+      // [TODO] I've never reached this line, but debug will be needed when this line executed during actual game
+      console.error("blockelement is null");
+      return;
+    }
+    const blockRect = (blockElement as HTMLElement).getBoundingClientRect();
+
+    dragPositionOffsetStore.set([
+      blockRect.left + Math.round($blockSizeStore / 2) - event.clientX,
+      blockRect.top + Math.round($blockSizeStore / 2) - event.clientY,
+    ]);
+  };
+
+  function handleDragEnd(event: DragEvent) {
+    const element = event.target as HTMLElement;
+    element.style.opacity = "1";
+    // [TODO] clear highlightedCells
+  }
 </script>
 
-<div class="block">
+<div
+  class="block"
+  ondragstart={handleDragStart}
+  ondragend={handleDragEnd}
+  draggable="true"
+  aria-label="block {blockType}"
+  role="grid"
+  tabindex="0"
+>
   <ColorMatrixRenderer id={`block-${blockType}`} matrix={blockMatrix}
   ></ColorMatrixRenderer>
 </div>
