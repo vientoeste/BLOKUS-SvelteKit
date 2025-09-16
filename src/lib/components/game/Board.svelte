8208026a6b270aaaf9d992c3f2f64a507f90af4a
vientoeste <djjd2000@naver.com>
2025-09-17 03:56:45 +0900

    Style: enhance accessibility attributes for #board-cover

--- src/lib/components/game/Board.svelte
+++ src/lib/components/game/Board.svelte
@@ -6,6 +6,12 @@
   let boardElement: HTMLElement;
 </script>
 
-<div id="board-cover" bind:this={boardElement}>
+<div
+  id="board-cover"
+  bind:this={boardElement}
+  role="grid"
+  tabindex="0"
+  aria-label="Game Board of Blokus"
+>
   <ColorMatrixRenderer id="board" matrix={$boardStore}></ColorMatrixRenderer>
 </div>
