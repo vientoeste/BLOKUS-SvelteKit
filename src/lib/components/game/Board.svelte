ad5062c200e12e7f732afa4a46340fa6552fc3ff
vientoeste <djjd2000@naver.com>
2025-09-18 15:38:27 +0900

    Feature: add newer version of getPosition to new component Board

--- src/lib/components/game/Board.svelte
+++ src/lib/components/game/Board.svelte
@@ -1,9 +1,36 @@
 <script lang="ts">
   // [TODO] DnD should be added
-  import { boardStore } from "$lib/store";
+  import {
+    blockSizeStore,
+    boardStore,
+    dragPositionOffsetStore,
+  } from "$lib/store";
   import ColorMatrixRenderer from "./ColorMatrixRenderer.svelte";
 
   let boardElement: HTMLElement;
+
+  const getPosition = ({
+    x,
+    y,
+  }: {
+    x: number;
+    y: number;
+  }): [number, number] => {
+    if (!boardElement)
+      throw new Error("board is not initialized until getPos be called");
+
+    const { left: boardLeft, top: boardTop } =
+      boardElement.getBoundingClientRect();
+
+    return [
+      Math.floor(
+        (y - boardTop + $dragPositionOffsetStore[1]) / ($blockSizeStore + 3),
+      ),
+      Math.floor(
+        (x - boardLeft + $dragPositionOffsetStore[0]) / ($blockSizeStore + 3),
+      ),
+    ];
+  };
 </script>
 
 <div
