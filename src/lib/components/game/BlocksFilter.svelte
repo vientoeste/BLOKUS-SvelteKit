714ab9cee469d8c189beabc67c6b5413f74a35ee
vientoeste <djjd2000@naver.com>
2025-09-22 16:21:50 +0900

    Feature: create BlocksFilter component with initial structure

---
+++ src/lib/components/game/BlocksFilter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div id="blocks-filter">
+  <div id="block-count-filter"></div>
+  <div id="block-color-filter"></div>
+</div>
+
+<style>
+  #blocks-filter {
+    height: fit-content;
+  }
+</style>
