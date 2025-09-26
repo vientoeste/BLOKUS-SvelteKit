e9362c6cd72ee7599c9ec9fcfaaf5bbd7d6d7b28
este <djjd2000@naver.com>
2025-09-26 18:13:23 +0900

    Docs: add jsdoc to function createFilter

--- src/lib/filter.svelte.ts
+++ src/lib/filter.svelte.ts
@@ -1,4 +1,21 @@
 // [TODO] move this function to proper position
+/**
+ * Creates a filter utility for managing selection state of a list of items.
+ *
+ * @template T The type of the items in the list.
+ * @param source A callback function that returns the array of items to be filtered(w. reactivity).
+ *
+ * @example
+ * const someFilter = createFilter<typeof foo>(() => $foo);
+ *
+ * @returns An object containing:
+ * - `items`: The current list of items.
+ * - `selected`: The currently selected items for props `checked` of checkbox `select all` and each checkboxes(by `filter.selected.includes(item)`).
+ * - `allSelected`: Whether all items are selected for props `checked` of checkbox `select all`.
+ * - `indeterminate`: Whether the selection is partial for props `indeterminate` of checkbox `select all`.
+ * - `toggleItem`: Function to toggle selection of a single item for props `onchange` of each checkbox(excludes `select all`).
+ * - `toggleAll`: Function to toggle selection of all items for props `onclick` of checkbox `select all`.
+ */
 export const createFilter = <T>(source: () => T[]) => {
   const items = $derived(source());
   let selected = $state([...items]);
