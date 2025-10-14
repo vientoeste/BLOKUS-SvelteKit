cd6b0f484dba8c34cda6ac6d2f936aca80167cb9
este <djjd2000@naver.com>
2025-10-14 17:38:58 +0900

    Feature: add container of chat

---
+++ src/lib/components/game/chat/Container.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { chatStore } from "$lib/store";
+  import ChatLog from "./Log.svelte";
+  let messagesViewModel = $derived.by(() =>
+    $chatStore.map((chat, idx) => ({
+      ...chat,
+      isSenderChanged:
+        idx === 0 || $chatStore[idx - 1].publisher !== chat.publisher,
+    })),
+  );
+</script>
+
+<ChatLog messages={messagesViewModel} />
