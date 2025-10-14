8e9ddfc0a8e360029c472c6d85b551a7de2fc54d
este <djjd2000@naver.com>
2025-10-14 17:37:36 +0900

    Feature: add component of chat message

---
+++ src/lib/components/game/chat/Message.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { ChatMessageViewModel } from "$types";
+
+  let { message } = $props<{ message: ChatMessageViewModel }>();
+</script>
+
+<div class="chat {message.isSystemMessage ? 'system-chat' : 'player-chat'}">
+  {#if message.isSenderChanged}
+    <div class="chat-publisher">{message.publisher}:</div>
+  {/if}
+  <div class="chat-content">{message.content}</div>
+</div>
+
+<style>
+  .chat {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    padding-left: 2px;
+  }
+
+  .player-chat {
+    padding: 2px;
+    background-color: #d9d9d9;
+    color: #000;
+    font-size: 14px;
+  }
+
+  .system-chat {
+    padding: 2px;
+    background-color: #989898;
+    color: #fff;
+    font-size: 16px;
+    justify-content: center;
+  }
+
+  .chat-content {
+    padding-left: 4px;
+  }
+</style>
