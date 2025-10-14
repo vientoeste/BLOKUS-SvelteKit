e3c0b9164e87ae43293db35965922426837de34e
este <djjd2000@naver.com>
2025-10-14 17:38:27 +0900

    Feature: add component of chat log displaying messages

---
+++ src/lib/components/game/chat/Log.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { ChatMessageViewModel } from "$types";
+  import ChatMessage from "./Message.svelte";
+
+  let { messages } = $props<{ messages: ChatMessageViewModel[] }>();
+</script>
+
+<div id="chat-log-container">
+  <div id="chat-log">
+    {#each messages as message (message.id)}
+      {#if message.isSenderChanged}
+        <hr />
+      {/if}
+      <ChatMessage {message} />
+    {/each}
+  </div>
+</div>
+
+<style>
+  #chat-log-container {
+    flex: 1;
+    height: 100%;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: var(--green);
+  }
+
+  #chat-log {
+    height: 100%;
+    background-color: #d9d9d9;
+  }
+</style>
