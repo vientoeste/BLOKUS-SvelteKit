f39dfd4532912fb532c2c2d381f75bae874b7882
vientoeste <djjd2000@naver.com>
2025-11-18 04:31:46 +0900

    Feature: add turn timer component

---
+++ src/lib/components/game/TurnTimer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { turnTimerProgress } from "$lib/store";
+
+  let { width = 30 } = $props();
+</script>
+
+<div
+  class="timer-container"
+  style="--progress: {1 - $turnTimerProgress}; width: {width}px;"
+>
+  <!-- [TODO] update background color by slot's color -->
+  <div class="timer-bar" style="background-color: #000"></div>
+</div>
+
+<style>
+  .timer-container {
+    height: 8px;
+    background-color: #eee;
+    border-radius: 5px;
+    overflow: hidden;
+    font-size: 10px;
+  }
+
+  .timer-bar {
+    height: 100%;
+    transform: scaleX(var(--progress));
+    transform-origin: left;
+  }
+</style>
