4c6ebc0f1c7a9b6f4f2e105580815727cba5e44b
este <djjd2000@naver.com>
2025-11-24 14:06:17 +0900

    Feature: add hook useGame

--- src/lib/client/game/context.ts
+++ src/lib/client/game/context.ts
@@ -1,4 +1,4 @@
-import { setContext } from 'svelte';
+import { setContext, getContext } from 'svelte';
 import type { GameStateLayer } from './application/facade';
 import type { GameManager } from './index';
 import { writable } from 'svelte/store';
@@ -18,5 +18,13 @@ export class GameContextContainer {
 export const createGameContext = () => {
   const container = new GameContextContainer();
   setContext(GAME_CTX_KEY, container);
+  return container;
+};
+
+export const useGame = () => {
+  const container = getContext<GameContextContainer>(GAME_CTX_KEY);
+  if (!container) {
+    throw new Error('useGame must be used within createGameContext scope');
+  }
   return container;
 };
