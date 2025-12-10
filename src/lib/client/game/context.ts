33044649b1637c0917b8b211fa394e58208caf6f
este <djjd2000@naver.com>
2025-12-10 23:30:09 +0900

    Feature: add presentation layer to the context

--- src/lib/client/game/context.ts
+++ src/lib/client/game/context.ts
@@ -1,5 +1,5 @@
 import { setContext, getContext } from 'svelte';
-import type { GameStateLayer } from './application/facade';
+import type { GamePresentationLayer, GameStateLayer } from './application/facade';
 import type { GameManager } from './index';
 import { writable } from 'svelte/store';
 
@@ -8,10 +8,16 @@ const GAME_CTX_KEY = Symbol('GAME_CTX');
 export class GameContextContainer {
   state = writable<GameStateLayer | undefined>(undefined);
   actions = writable<GameManager | undefined>(undefined);
+  presentation = writable<GamePresentationLayer | undefined>(undefined);
 
-  initialize({ state, actions }: { state: GameStateLayer, actions: GameManager }) {
+  initialize({ state, actions, presentation }: {
+    state: GameStateLayer;
+    actions: GameManager;
+    presentation: GamePresentationLayer;
+  }) {
     this.state.set(state);
     this.actions.set(actions);
+    this.presentation.set(presentation);
   }
 }
 
